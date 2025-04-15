a1c84daa92cbc36817975fda1778c3b2ef5fdf68
Mohamed <mohamedboussas0@gmail.com>
2025-04-15 11:25:01 +0100

    feat: ajout de Calculatrice et CalculatriceTest

---
+++ src/main/java/com/example/Calculatrice.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Calculatrice {
+    public int addition(int a, int b) { return a + b; }
+    public int soustraction(int a, int b) { return a - b; }
+    public int multiplication(int a, int b) { return a * b; }
+    public int division(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division par zéro");
+        return a / b;
+    }
+    public int puissance(int a, int b) {
+        if (b < 0) throw new IllegalArgumentException("Exposant négatif non autorisé");
+        return (int) Math.pow(a, b);
+    }
+}
